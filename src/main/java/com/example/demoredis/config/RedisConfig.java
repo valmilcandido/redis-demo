b41488be71724f2e3854f1a4be8a5f174f7a28ff
Valmil <valmil.silva@sulamerica.com.br>
2022-09-13 00:33:40 -0300

    First commit

---
+++ src/main/java/com/example/demoredis/config/RedisConfig.java
@@ -0,0 +1,22 @@
+package com.example.demoredis.config;
+
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public void redisFactory(){
+        RedisClient redisClient = RedisClient.create("redis://password@10.89.101.131:6379");
+        StatefulRedisConnection<String, String> connection = redisClient.connect();
+        RedisCommands<String, String> syncCommands = connection.sync();
+
+        syncCommands.set("key", "Hello, Redis!");
+
+        connection.close();
+        redisClient.shutdown();
+    }
+}
